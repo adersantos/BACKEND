dde0fdb761e03a47f7ac17c819ce29556a3ed331
Ader Santos <adersantos@yahoo.com.br>
2022-10-07 12:12:58 -0300

    Create GetEventoByIdQuery.cs

---
+++ teste-squadra-api/Queries/GetEventoByIdQuery.cs
@@ -0,0 +1,12 @@
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_squadra_api.Queries
+{
+    public record GetEventoByIdQuery(int id) : IRequest<Evento>;
+ 
+}
