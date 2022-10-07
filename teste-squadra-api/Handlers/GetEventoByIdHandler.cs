264636bfadd006136768c5f3a416e5f53e356da3
Ader Santos <adersantos@yahoo.com.br>
2022-10-07 12:10:21 -0300

    Add files via upload

---
+++ teste-squadra-api/Handlers/GetEventoByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_squadra_api.Handlers
+{
+    public class GetEventoByIdHandler : IRequestHandler<GetEventoByIdQuery, EventoModel>
+    {
+        private readonly IMediator _mediator;
+
+        public GetEventoByIdHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<EventoModel> Handle(GetEventoByIdQuery request, CancellationToken cancellationToken)
+        {
+            var Eventos = await _mediator.Send(new GetEventoListQuery());
+            var Evento = Eventos.FirstOrDefault(u => u.Id == request.id);
+            return Evento;
+        }
+    }
+}
---
+++ teste-squadra-api/Handlers/GetEventoListHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace teste_squadra_api.Handlers
+{
+    public class GetEventoListHandler : IRequestHandler<GetEventoListQuery, List<EventoModel>>
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public GetEventoListHandler(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+        public Task<List<EventoModel>> Handle(GetEventoListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_dataRepository.GetEventos());
+        }
+    }
+}
