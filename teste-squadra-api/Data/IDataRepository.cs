483a34d4aabe97c523a540500ba12adee727296b
Ader Santos <adersantos@yahoo.com.br>
2022-10-07 12:08:03 -0300

    Add files via upload

---
+++ teste-squadra-api/Data/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste_squadra_api.Data
+{
+    public interface IDataRepository
+    {
+        List<Evento> GetEventoss();
+        Evento AddEento(Evento evento);
+    }
+}
